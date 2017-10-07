4337fef9a94686929c2424c5ea2a73184e266a21
王李林 <wll_sx1990@163.com>
2017-10-07 15:55:29 +0800

    【项目】目录结构修改

--- IM/client/app/src/main/java/wll/com/imclient/net/Configs.java
+++
@@ -1,7 +0,0 @@
-package wll.com.imclient.net;
-
-public class Configs {
-    public static final String SERVER_IP = "192.168.1.2";
-    public static final int SERVER_PORT = 10010;
-    public static final int TIMEOUT = 5000;
-}
